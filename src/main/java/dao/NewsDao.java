53da1fa23b90ba95c34256823d33974c48a153a8
pkminor <minorpk@gmail.com>
2019-11-30 12:28:49 +0300

    completed User, News, Department dao tests

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,27 @@
 package dao;
 
+import models.Department;
+import models.DepartmentNews;
+import models.News;
+
+import java.util.List;
+
 public interface NewsDao {
+
+    List<News> getAllNews();
+    List<News> getGeneralNews();
+    List<DepartmentNews> getDepartmentNews();
+
+    void addGeneralNews(News news);
+    void addDepartmentNews(DepartmentNews dptNews);
+
+    News findGeneralNewsById(int id);
+    DepartmentNews findDepartmentNewsById(int id);
+
+    void updateGeneralNews(News news, int userId, String content);
+    void updateDepartmentNews(DepartmentNews dptNews, int userId, String content, int departmentId);
+
+    void clearAllNews();
+    void clearGeneralNews();
+    void clearDepartmentNews();
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,13 @@
 package dao;
 
+import models.User;
+
+import java.util.List;
+
 public interface UserDao {
+    List<User> getAllUsers();
+    void addUser(User user);
+    User findUserById(int id);
+    void updateUser(User user, String name, String position,String role,int departmentId);
+    void clearAllUsers();
 }
