10efed55908d6c70fae25bb8338f92d1e032d458
pkminor <minorpk@gmail.com>
2019-11-30 12:45:13 +0300

    added and implemented department dao

--- src/main/java/dao/Sql2oDepartmentDao.java
+++ src/main/java/dao/Sql2oDepartmentDao.java
@@ -1,4 +1,93 @@
 package dao;
 
-public class Sql2oDepartmentDao {
+import models.Department;
+import models.DepartmentNews;
+import models.News;
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Sql2oDepartmentDao implements DepartmentDao {
+
+    private final Sql2o sql2o;
+    private final Sql2oUserDao userDao;
+    private final Sql2oNewsDao newsDao;
+
+    public Sql2oDepartmentDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+        this.userDao = new Sql2oUserDao(sql2o);
+        this.newsDao = new Sql2oNewsDao(sql2o);
+    }
+
+    @Override
+    public List<Department> getAllDepartments() {
+        String sql ="select * from departments";
+        try(Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(Department.class);
+        }
+
+    }
+
+    @Override
+    public List<User> getDepartmentUsersById(int id) {
+        return userDao.getAllUsers().stream()
+                .filter(user -> user.getDepartmentId()==id )
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<DepartmentNews> getDepartmentNewsById(int id) {
+        return newsDao.getDepartmentNews().stream()
+                .filter(dpt->dpt.getDepartmentId()==id)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public void addDepartment(Department department) {
+        String sql = "insert into departments (name,description) values (:name,:description) ";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql,true)
+                             .bind(department)
+                             .executeUpdate()
+                             .getKey();
+            department.setId(id);
+        }
+    }
+
+    @Override
+    public Department findDepartmentById(int id) {
+        String sql ="select * from departments where id=:id";
+        try(Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(Department.class);
+        }
+
+    }
+
+    @Override
+    public void updateDepartment(Department department, String name, String description) {
+        String sql ="update departments set (name, description) = (:name, :description) ";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("name",name)
+                    .addParameter("description",description)
+                    .executeUpdate();
+            department.setName(name);
+            department.setDescription(description);
+        }
+    }
+
+    @Override
+    public void clearAllDepartments() {
+        String sql =" delete from departments";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql).executeUpdate();
+        }
+    }
 }
